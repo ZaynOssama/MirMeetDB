0b45eed0aaab901b2bb88a32e162730af7e57a23
Tud0r420 <94829026+Tud0r420@users.noreply.github.com>
2023-02-24 16:27:34 +0100

    update

--- factories/FollowFactory.php
+++ factories/FollowFactory.php
@@ -2,22 +2,20 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Follow>
- */
 class FollowFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+
     public function definition()
     {
+        $follow = User::all()->pluck('id')->toArray();
+
         return [
-            //
+            'follower_id' => $this->faker->randomElement($follow),
+            'following_id' => $this->faker->randomElement($follow),
         ];
     }
 }
