d24ce68ba22f8fc1e7b2a44666736f477e5d766a
ZaynOssama <94825975+ZaynOssama@users.noreply.github.com>
2023-02-24 16:09:25 +0100

    Update 10_create_comment_table.php

--- migrations/10_create_comment_table.php
+++ migrations/10_create_comment_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('sentby_id');
             $table->unsignedBigInteger('sento_id');
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('messages');
     }
 };
