6792e87e59a2c1cd4b4b2bfd41fe5ab18c8a2ad0
ZaynOssama <94825975+ZaynOssama@users.noreply.github.com>
2023-02-24 16:31:28 +0100

    Update and rename Notificacio.php to Notification.php

---
+++ Models/Notification.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notificacio extends Model
+{
+    use HasFactory;
+        
+    protected $table = "notifications";
+
+    protected $fillable = [
+        'message_id',
+        'share_id',
+        'publication_id',
+        'like_id',
+        'user_id',
+    ];
+}
